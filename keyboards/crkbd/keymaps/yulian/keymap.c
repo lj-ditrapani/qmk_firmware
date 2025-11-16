75c9c550a8cb09c0ea05be72cc744e07be27c6a6
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2025-11-16 12:14:58 -0500

    feat(crkbd): Add more combos and tweak combos
    Also, format keymap more.

--- keyboards/crkbd/keymaps/yulian/keymap.c
+++ keyboards/crkbd/keymaps/yulian/keymap.c
@@ -13,12 +13,18 @@ const uint16_t PROGMEM combo_j[] = {RGUI_T(KC_S), RSFT_T(KC_T), COMBO_END};
 const uint16_t PROGMEM combo_x[] = {RALT_T(KC_R), RGUI_T(KC_S), COMBO_END};
 const uint16_t PROGMEM combo_x2[] = {KC_F, KC_C, COMBO_END};
 const uint16_t PROGMEM combo_x3[] = {RCTL_T(KC_H), RGUI_T(KC_S), COMBO_END};
-const uint16_t PROGMEM combo_lparen[] = {KC_SLSH, KC_MINS, COMBO_END};
-const uint16_t PROGMEM combo_rparen[] = {KC_EXLM, KC_AT, COMBO_END};
-const uint16_t PROGMEM combo_lbrack[] = {KC_GT, KC_COLN, COMBO_END};
-const uint16_t PROGMEM combo_rbrack[] = {KC_DLR, KC_PERC, COMBO_END};
-const uint16_t PROGMEM combo_lcbrck[] = {KC_QUES, KC_UNDS, COMBO_END};
-const uint16_t PROGMEM combo_rcbrck[] = {KC_AMPR, KC_ASTR, COMBO_END};
+const uint16_t PROGMEM combo_lparen[] = {KC_DOT, KC_M, COMBO_END};
+const uint16_t PROGMEM combo_rparen[] = {KC_D, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_lbrack[] = {KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM combo_rbrack[] = {KC_W, KC_P, COMBO_END};
+const uint16_t PROGMEM combo_lcbrck[] = {KC_Y, KC_U, COMBO_END};
+const uint16_t PROGMEM combo_rcbrck[] = {KC_C, KC_G, COMBO_END};
+const uint16_t PROGMEM combo_lparen2[] = {KC_SLSH, KC_MINS, COMBO_END};
+const uint16_t PROGMEM combo_rparen2[] = {KC_EXLM, KC_AT, COMBO_END};
+const uint16_t PROGMEM combo_lbrack2[] = {KC_GT, KC_COLN, COMBO_END};
+const uint16_t PROGMEM combo_rbrack2[] = {KC_DLR, KC_PERC, COMBO_END};
+const uint16_t PROGMEM combo_lcbrck2[] = {KC_QUES, KC_UNDS, COMBO_END};
+const uint16_t PROGMEM combo_rcbrck2[] = {KC_AMPR, KC_ASTR, COMBO_END};
 combo_t key_combos[] = {
     COMBO(combo_k, KC_K),
     COMBO(combo_q, KC_Q),
@@ -36,6 +42,12 @@ combo_t key_combos[] = {
     COMBO(combo_rbrack, KC_RBRC),
     COMBO(combo_lcbrck, KC_LCBR),
     COMBO(combo_rcbrck, KC_RCBR),
+    COMBO(combo_lparen2, KC_LPRN),
+    COMBO(combo_rparen2, KC_RPRN),
+    COMBO(combo_lbrack2, KC_LBRC),
+    COMBO(combo_rbrack2, KC_RBRC),
+    COMBO(combo_lcbrck2, KC_LCBR),
+    COMBO(combo_rcbrck2, KC_RCBR),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,40 +64,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(6,KC_ESC), LT(2,KC_E), LT(4,KC_TAB),                                                LT(5,KC_ENT), LT(3,KC_SPC), LT(7,KC_BSPC)
     ),
     [2] = LAYOUT_split_3x6_3_ex2(
-    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,    KC_NO, KC_TAB, KC_4, KC_5, KC_6, KC_NO, KC_NO,
-    KC_NO, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, KC_NO, KC_NO,    KC_NO, KC_EQL, KC_1, KC_2, KC_3, KC_0,  KC_NO,
-    KC_NO, KC_NO,   KC_NO,   KC_NO,   MS_BTN3, KC_NO,                  KC_DOT, KC_7, KC_8, KC_9, KC_NO, KC_NO,
-    KC_NO, KC_NO, KC_NO,                                               KC_ENT, KC_SPC, KC_BSPC
+    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO,  KC_4, KC_5, KC_6, KC_NO, KC_NO,
+    KC_NO, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, KC_NO, KC_NO,    KC_NO, KC_NO,  KC_1, KC_2, KC_3, KC_0,  KC_NO,
+    KC_NO, KC_NO,   KC_NO,   KC_NO,   MS_BTN3, KC_NO,                  KC_NO,  KC_7, KC_8, KC_9, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO,                                               KC_ENT, KC_0, KC_DOT
     ),
     [3] = LAYOUT_split_3x6_3_ex2(
-    KC_NO, TO(8),   KC_HOME, KC_UP,   KC_END,  TO(3),   KC_NO,      KC_NO, KC_NO, TO(0),   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO, MS_BTN5, KC_LEFT, KC_DOWN, KC_RGHT, MS_BTN4, KC_NO,      KC_NO, KC_NO, KC_RCTL, KC_RSFT, KC_RGUI, KC_RALT, KC_NO,
-    KC_NO, KC_NO,   KC_NO,   KC_PGDN, KC_PGUP, KC_NO,                      KC_NO, TO(1),   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+    KC_NO, TO(8),   KC_HOME, KC_UP,   KC_END,  KC_NO, KC_NO,    KC_NO, KC_NO, TO(0),   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_NO, TO(3),   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO,    KC_NO, KC_NO, KC_RCTL, KC_RSFT, KC_RGUI, KC_RALT, KC_NO,
+    KC_NO, MS_BTN5, MS_BTN4, KC_PGDN, KC_PGUP, KC_NO,                  KC_NO, TO(1),   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_NO, KC_NO,   KC_NO,                                             KC_NO, KC_NO, KC_NO
     ),
     [4] = LAYOUT_split_3x6_3_ex2(
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RBRC, KC_DLR, KC_PERC, KC_CIRC, KC_NO, KC_NO,
-    KC_NO, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, KC_NO, KC_NO, KC_NO, KC_RPRN, KC_EXLM, KC_AT, KC_HASH, KC_EQL, KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RCBR, KC_AMPR, KC_ASTR, KC_PLUS, KC_NO, KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_DEL
+    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_DLR,  KC_PERC, KC_CIRC, KC_NO,  KC_NO,
+    KC_NO, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, KC_NO, KC_NO,    KC_NO, KC_NO, KC_EXLM, KC_AT,   KC_HASH, KC_EQL, KC_NO,
+    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                  KC_NO, KC_AMPR, KC_ASTR, KC_PLUS, KC_NO,  KC_NO,
+    KC_NO, KC_NO, KC_NO,                                               KC_NO, KC_NO, KC_DEL
     ),
     [5] = LAYOUT_split_3x6_3_ex2(
-    KC_NO, KC_DQUO, KC_LT, KC_GT, KC_COLN, KC_LBRC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_GRV, KC_BSLS, KC_SLSH, KC_MINS, KC_LPRN, KC_NO, KC_NO, KC_NO, KC_RCTL, KC_RSFT, KC_RGUI, KC_RALT, KC_NO,
-    KC_NO, KC_TILD, KC_PIPE, KC_QUES, KC_UNDS, KC_LCBR, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+    KC_NO, KC_DQUO, KC_LT,   KC_GT,   KC_COLN, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_NO, KC_GRV,  KC_BSLS, KC_SLSH, KC_MINS, KC_NO, KC_NO,    KC_NO, KC_NO, KC_RCTL, KC_RSFT, KC_RGUI, KC_RALT, KC_NO,
+    KC_NO, KC_TILD, KC_PIPE, KC_QUES, KC_UNDS, KC_NO,                  KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_NO, KC_NO, KC_NO,                                               KC_NO, KC_NO, KC_NO
     ),
     [6] = LAYOUT_split_3x6_3_ex2(
-    KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F4, KC_F5, KC_F6, KC_NO, KC_NO,
-    KC_NO, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_NO, KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_F12, KC_F11
+    KC_NO, QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_F4, KC_F5, KC_F6, KC_NO,  KC_NO,
+    KC_NO, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, KC_NO, KC_NO,    KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_NO,  KC_NO,
+    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                  KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO,
+    KC_NO, KC_NO, KC_NO,                                               KC_NO, KC_F12, KC_F11
     ),
     [7] = LAYOUT_split_3x6_3_ex2(
-    KC_NO, KC_NO, KC_APP, MS_UP, RM_VALU, RM_VALD, KC_NO, KC_NO, KC_NO, RM_TOGG, RM_SATU, RM_SATD, QK_BOOT, KC_NO,
-    KC_NO, MS_BTN5, MS_LEFT, MS_DOWN, MS_RGHT, MS_BTN4, KC_NO, KC_NO, RM_SPDU, KC_RCTL, KC_RSFT, KC_RGUI, KC_RALT, KC_NO,
-    KC_NO, KC_NO, MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, RM_SPDD, RM_NEXT, RM_PREV, RM_HUEU, RM_HUED, KC_NO,
-    MS_BTN2, MS_BTN1, MS_BTN3, KC_NO, KC_NO, KC_NO
+    KC_NO, KC_NO,   KC_APP,  MS_UP,   RM_VALU, RM_VALD, KC_NO,  KC_NO, KC_NO,   RM_TOGG, RM_SATU, RM_SATD, QK_BOOT, KC_NO,
+    KC_NO, MS_BTN5, MS_LEFT, MS_DOWN, MS_RGHT, MS_BTN4, KC_NO,  KC_NO, RM_SPDU, KC_RCTL, KC_RSFT, KC_RGUI, KC_RALT, KC_NO,
+    KC_NO, MS_WHLL, MS_WHLR, MS_WHLD, MS_WHLU, KC_NO,                  RM_SPDD, RM_NEXT, RM_PREV, RM_HUEU, RM_HUED, KC_NO,
+    MS_BTN2, MS_BTN1, MS_BTN3,                                         KC_NO, KC_NO, KC_NO
     ),
     [8] = LAYOUT_split_3x6_3_ex2(
     KC_NO, KC_T, KC_Q, KC_W, KC_E, KC_R, KC_NO, KC_NO, KC_Y, KC_U, KC_UP, KC_O, KC_P, KC_NO,
